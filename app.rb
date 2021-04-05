2843667a32c3e3b4e72cab5b42649f8a687f474e
Tyler Maxwell <tylermaxwell661@gmail.com>
2021-04-05 18:03:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,11 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
+<<<<<<< HEAD
     "Started my server using shotgun"
+=======
+    "Welcome to your app!!!! I BUILT THIS!"
+>>>>>>> bb2b94ec45d7ec8e64b2df4a26f4b6836fea43b9
   end
 
 end
